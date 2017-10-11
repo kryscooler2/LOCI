bc86f5525004bbb1c21cd50a03779e08e6897be5
vrojo <vin.rojo@gmail.com>
2017-10-11 16:04:46 +0200

    Base Android Project
    Just import the main android project.

---
+++ LOCI/app/src/main/java/lociteam/com/loci/Search.java
@@ -0,0 +1,21 @@
+package lociteam.com.loci;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class Search extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
