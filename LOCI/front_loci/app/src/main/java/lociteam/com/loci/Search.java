6c55b8bf0e4006aec34beea3a87d68a97a8687b9
linfengisep <wang.linfeng0213@gmail.com>
2017-11-01 22:48:42 +0100

    Toast

--- LOCI/front_loci/app/src/main/java/lociteam/com/loci/Search.java
+++ LOCI/front_loci/app/src/main/java/lociteam/com/loci/Search.java
@@ -1,6 +1,7 @@
 package lociteam.com.loci;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class Search extends AppCompatActivity {
@@ -32,6 +34,14 @@ public class Search extends AppCompatActivity {
         searchButton.setOnClickListener( new View.OnClickListener(){
             @Override
             public void onClick(View viewResult){
+                //dès que l'on clique le boutton, il va s'afficher le text="le boutton d'envoyer est déclenché.";
+                Context context = getApplicationContext();
+                CharSequence text="The shortest path will show immediately";
+                int duration= Toast.LENGTH_SHORT;
+                Toast toast=Toast.makeText(context,text,duration);
+                toast.show();
+
+                //change pour l'activité MapResult;
                 Intent ResultScreen= new Intent(Search.this, MapResult.class);
                 //envoyez les donnes à deuxième activité.
                 ResultScreen.putExtra("departure",departureStation.getText().toString());
