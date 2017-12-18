eebe71d729e0db3aae2bfd74919104533956e8b4
linfengisep <wang.linfeng0213@gmail.com>
2017-12-18 22:35:27 +0100

    change geolocation

--- LOCI/front_loci/app/src/main/java/lociteam/com/loci/MapsActivity.java
+++ LOCI/front_loci/app/src/main/java/lociteam/com/loci/MapsActivity.java
@@ -1,105 +1,38 @@
 package lociteam.com.loci;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
+public class MapsActivity extends Activity {
 
-public class MapsActivity extends FragmentActivity implements LocationListener {
-    private LocationManager locationManager;
-    private GoogleMap gMap;
-    private Marker marker;
+    private Integer images [] = {R.mipmap.metro,R.mipmap.paris};
+    //R.drawable.pic2, R.drawable.pic3
 
     @Override
-    protected void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
-        //gMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-        marker = gMap.addMarker(new MarkerOptions().title("Vous êtes ici").position(new LatLng(0, 0)));
+        addImagesToThegallery();
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            abonnementGPS();
+    private void addImagesToThegallery() {
+        LinearLayout imageGallery = (LinearLayout)findViewById(R.id.imageGallery);
+        for(Integer image : images) {
+            imageGallery.addView(getImageView(image));
         }
+
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-
-        desabonnementGPS();
+    private View getImageView(Integer image) {
+        ImageView imageView = new ImageView(getApplicationContext());
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(0, 0, 10, 0);
+        imageView.setLayoutParams(lp);
+        imageView.setImageResource(image);
+        return imageView;
     }
 
-    public void abonnementGPS() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, this);
-    }
-
-
-    public void desabonnementGPS() {
-        //Si le GPS est disponible, on s'y abonne
-        locationManager.removeUpdates(this);
-    }
-
-
-    @Override
-    public void onLocationChanged(final Location location) {
-        final StringBuilder msg = new StringBuilder("lat : ");
-        msg.append(location.getLatitude());
-        msg.append( "; lng : ");
-        msg.append(location.getLongitude());
-
-        Toast.makeText(this, msg.toString(), Toast.LENGTH_SHORT).show();
-
-
-        final LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
-        marker.setPosition(latLng);
-    }
-
-
-    @Override
-    public void onProviderDisabled(final String provider) {
-        if("gps".equals(provider)) {
-            desabonnementGPS();
-        }
-    }
-
-
-    @Override
-    public void onProviderEnabled(final String provider) {
-        if("gps".equals(provider)) {
-            abonnementGPS();
-        }
-    }
-
-    @Override
-    public void onStatusChanged(final String provider, final int status, final Bundle extras) { }
 }
-
